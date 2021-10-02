53df965f103106740e3505ca929ccd027a1fe348
Olesya Tretyakova <OTretyakova@beeline.kz>
2021-10-02 12:56:13 +0600

    add new AllureTest

---
+++ src/test/java/home/artreiner/tests/AllureTest.java
@@ -0,0 +1,19 @@
+package home.artreiner.tests;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class AllureTest {
+    String baseUrl = "https://github.com";
+
+    @Test
+    void searchAllureRepoOnGitHub() {
+        open(baseUrl);
+        $(By.name("q")).setValue("allure").pressEnter();
+        $(".repo-list").shouldHave(Condition.text("allure"));
+    }
+}
